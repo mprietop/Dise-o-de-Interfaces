ea4a9021ece376cae7e9cde3747657b3e72a0c7b
Mario Prieto <mprietop@fpcoruna.afundacion.org>
2022-09-27 17:47:03 +0200

    Sprint 4

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,4 +1,14 @@
-from fibonacci2 import funcion_fibonacci
+from fibonacci import funcion_fibonacci as fibonacci
+from fibonacci2 import funcion_fibonacci as fibonacci2
+
 
 n = int(input("Introduzca el número: "))
-print(funcion_fibonacci(n))
+op = input("Que método prefiere utilizar? a) b)")
+
+while (op != 'a') & (op != 'b'):
+    op = input("Que método prefiere utilizar? a) b)")
+    
+if op == 'a':
+    print(fibonacci(n))
+else:
+    print(fibonacci2(n))
