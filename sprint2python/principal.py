359a7769d4755c4a26b75cfa89067f178bfe432c
Mario Prieto <mprietop@fpcoruna.afundacion.org>
2022-09-27 19:34:32 +0200

    Sprint 5

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,6 +1,6 @@
 from fibonacci import funcion_fibonacci as fibonacci
 from fibonacci2 import funcion_fibonacci as fibonacci2
-
+import time
 
 n = int(input("Introduzca el número: "))
 op = input("Que método prefiere utilizar? a) b)")
@@ -9,6 +9,15 @@ while (op != 'a') & (op != 'b'):
     op = input("Que método prefiere utilizar? a) b)")
     
 if op == 'a':
+    start_time = time.time()
     print(fibonacci(n))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+    
 else:
+    start_time = time.time()
     print(fibonacci2(n))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
