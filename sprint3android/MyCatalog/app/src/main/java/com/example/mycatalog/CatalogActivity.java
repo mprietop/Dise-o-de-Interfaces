f1cd9d7f7043dd03480e56770c469d164cb02c04
Mario Prieto <mprietop@fpcoruna.afundacion.org>
2022-11-07 17:43:37 +0100

    Tarea 3

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -8,18 +8,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
+    //Creamos una variable de tipo Button para poder trabajar con ella en java
     private Button navButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        //Asociamos el Button del xml al de java mediante el ID
         navButton = findViewById(R.id.navButton);
+        //Creamos un intent para poder llamar a una actividad nueva
         Intent intent = new Intent(this, DetailActivity.class);
 
         navButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Creamos un onClickListener para que cuando detecte que se pulsa el boton
+                //entra ahí y comienza la actividad asociada al intent
                 startActivity(intent);
             }
         });
--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -3,12 +3,19 @@ package com.example.mycatalog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
+    //Creamos una variable de tipo ImageView para poder trabajar con ella en java
+    private ImageView imagePrenda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        //Asociamos el ImageView del xml al de java mediante el ID
+        imagePrenda = findViewById(R.id.imagePrenda);
+        //Ponermos el ScaleType como Center crop
+        ImageView.ScaleType centerCrop = ImageView.ScaleType.CENTER_CROP;
     }
 }
