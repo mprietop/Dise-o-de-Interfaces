87e9bffa35e158382e75256208726a23db06fa91
Mario Prieto <mprietop@fpcoruna.afundacion.org>
2022-11-07 11:56:17 +0100

    Tarea 2

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,7 +2,9 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
@@ -13,5 +15,13 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
         navButton = findViewById(R.id.navButton);
+        Intent intent = new Intent(this, DetailActivity.class);
+
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
